245c53f814a7375fcb16d5ca902bf779115672e7
njberman <njberman16@hotmail.com>
2021-02-04 13:31:55 +0000

    :sparkles: Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# njberman.github.io
+
+> This is the repo for my website [njberman.github.io](https://njberman.github.io)
+
+This is the repo in which my website is hosted.
+It is hosted on github pages, which you can learn about [here](https://pages.github.com/).
+Thank you to GitHub for being amazing!
+🙌🙌🙌
---
+++ home/home.js
@@ -0,0 +1,107 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable consistent-return */
+/* eslint-disable no-return-assign */
+const SPEED_OF_ANIMATION = 0.5;
+const GRID_SIZE = 25;
+
+/**
+ * Key:
+ *  ^b = backspace
+ *    deletes character two behind `^`
+ *    keeps character one behind `^`
+ *    deletes `^b` from string
+ */
+
+let TEXT = 'n^bjbermga^bn';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const animations = document.getElementById('animations');
+  const textElem = document.getElementById('title');
+  animations.style.gridTemplateColumns = `repeat(${GRID_SIZE}, 1fr)`;
+  animations.style.gridTemplateRows = `repeat(${GRID_SIZE}, 1fr)`;
+  // animations.innerHTML = '';
+  let animationsArray = Array.from(
+    { length: GRID_SIZE },
+    () => Array.from(
+      { length: GRID_SIZE },
+      () => {
+        const a = document.createElement('div');
+        a.classList.add('animation');
+        animations.appendChild(a);
+        return a;
+      },
+    ),
+  );
+  let x = 0;
+  let y = 0;
+  function showAnimation(mode) {
+    console.log(mode);
+    if (mode === 'ORDERED') {
+      if (x !== 0) {
+        animationsArray[y][x - 1].style.animation = 'fade ease 10s';
+      }
+      x += 1;
+      if (y === GRID_SIZE) {
+        y = 0;
+        animations.innerHTML = '';
+        animationsArray = Array.from(
+          { length: GRID_SIZE },
+          () => Array.from(
+            { length: GRID_SIZE },
+            () => {
+              const a = document.createElement('div');
+              a.classList.add('animation');
+              animations.appendChild(a);
+              return a;
+            },
+          ),
+        );
+      }
+      if (x === GRID_SIZE + 1) {
+        x = 0;
+        y += 1;
+      }
+    } else if (mode === 'RANDOM') {
+      x = Math.floor(Math.random() * GRID_SIZE);
+      y = Math.floor(Math.random() * GRID_SIZE);
+      animationsArray[y][x].style.animation = 'fade ease 10s';
+    }
+  }
+
+  const textInterval = setInterval(() => {
+    showText();
+  }, SPEED_OF_ANIMATION * 1000);
+  setInterval(blink, (SPEED_OF_ANIMATION / 2) * 1000);
+
+  let i = 0;
+  function showText() {
+    if (i !== -1) {
+      textElem.innerHTML = `${TEXT.slice(0, i)}<span id="blinker">|</span>`;
+    }
+    i += 1;
+    if (i === TEXT.length + 1) {
+      TEXT = 'njbce^brmga^bn';
+      clearInterval(textInterval);
+      return i = 0;
+    }
+    if (TEXT[i] === '^' && TEXT[i + 1] === 'b') {
+      TEXT = TEXT.replace('^b', '');
+      TEXT = TEXT.replace(TEXT[i - 2], '');
+      return i -= 2;
+    }
+  }
+
+  let on = true;
+  function blink() {
+    const blinker = document.getElementById('blinker');
+    blinker.style.visibility = on ? 'unset' : 'hidden';
+    on = !on;
+  }
+
+  // showAnimation();
+  // showText();
+  setInterval(() => {
+    // showAnimation('ORDERED');
+    showAnimation('RANDOM');
+  }, SPEED_OF_ANIMATION * 1000);
+});
