db264d464193536c494a705faebcb97c87c4e33b
njberman <67945050+njberman@users.noreply.github.com>
2021-06-24 16:09:36 +0100

    Add Angry Browsers

---
+++ Angry-Browsers/js/Cannon.js
@@ -0,0 +1,108 @@
+/**
+ * Cannon.js
+ *
+ * @author ktoso
+ * @date 16.04.11
+ * @version 1
+ */
+
+var Cannon = Class.extend({
+
+    force: null,
+
+    browserSize: 0.7,
+
+
+    x: 5,
+    y: 15,
+
+    browser: null,
+    browsers: [
+
+    ],
+
+    game: null,
+    fixDef: null,
+
+    input_power: null,
+    input_angle: null,
+
+    can: null,
+
+    init: function(game) {
+
+        this.input_power = document.getElementById('power');
+        this.input_angle = document.getElementById('angle');
+
+        //todo clean me
+        var b2Vec2 = Box2D.Common.Math.b2Vec2,
+                b2AABB = Box2D.Collision.b2AABB,
+                b2BodyDef = Box2D.Dynamics.b2BodyDef,
+                b2Body = Box2D.Dynamics.b2Body,
+                b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
+                b2Fixture = Box2D.Dynamics.b2Fixture,
+                b2World = Box2D.Dynamics.b2World,
+                b2MassData = Box2D.Collision.Shapes.b2MassData,
+                b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+                b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
+                b2DebugDraw = Box2D.Dynamics.b2DebugDraw,
+                b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef;
+
+        this.game = game;
+
+        this.browser = new b2FixtureDef;
+        this.browser.density = 1.0;
+        this.browser.mass = 1.0;
+        this.browser.friction = 0.5;
+        this.browser.restitution = 0.2;
+
+        this.force = b2Vec2(1000, 1000);
+
+        this.nextBrowser();
+        this.initEventHandlers();
+    },
+
+    nextBrowser: function() {
+        var b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
+            b2Body = Box2D.Dynamics.b2Body;
+
+        this.game.bodyDef.type = b2Body.b2_kinematicBody;
+        this.browser.shape = new b2CircleShape(this.browserSize);
+
+        this.game.bodyDef.position.Set(this.x, this.y);
+
+//        this.game.world.CreateBody(this.game.bodyDef).CreateFixture(this.browser);
+        this.browserBrowser = this.game.world.CreateBody(this.game.bodyDef);
+        this.browserFixture = this.browserBrowser.CreateFixture(this.browser);
+
+        var aabb = new Box2D.Collision.b2AABB(), that = this;
+        aabb.lowerBound.Set(this.x - 0.001, this.y - 0.001);
+        aabb.upperBound.Set(this.x + 0.001, this.y + 0.001);
+        this.game.world.QueryAABB(function (fixture) {
+            if(fixture.GetBody().GetType() != b2Body.b2_staticBody) {
+               if(fixture.GetShape().TestPoint(fixture.GetBody().GetTransform(), new Box2D.Common.Math.b2Vec2(that.x,that.y))) {
+                  that.can = fixture.GetBody();
+                  return false;
+               }
+            }
+            return true;
+         }, aabb);
+    },
+
+    initEventHandlers: function() {
+        var self = this, b2Body = Box2D.Dynamics.b2Body;
+        this.game.canvas.addEventListener("click", function(event) {
+            var angle = self.input_angle.value * -1;
+
+            var power = {
+                x: Math.cos(Math.PI * angle/180) * self.input_power.value,
+                y: Math.sin(Math.PI * angle/180) * self.input_power.value
+            };
+            if(power.x == 0) {power.x = 0.01;}
+            if(power.y == 0) {power.y = 0.01;}
+
+            self.can.SetType(b2Body.b2_dynamicBody);
+            self.can.ApplyImpulse(power, self.can.position);
+        }, false);
+    }
+});
---
+++ Angry-Browsers/js/Maps.js
@@ -0,0 +1,155 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Sahadar
+ * Date: 16.04.11
+ * Time: 12:29
+ * To change this template use File | Settings | File Templates.
+ */
+
+var Maps = Class.extend({
+    game: null,
+
+    maps: {
+        random: function() {
+            var Game = this.game;
+            var b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+                b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
+                b2Body = Box2D.Dynamics.b2Body;
+            
+            Game.bodyDef.type = b2Body.b2_dynamicBody;
+//            Game.bodyDef.type = b2Body.b2_dynamicBody;
+//            Game.bodyDef.type = b2Body.b2_staticBody;
+//            Game.bodyDef.type = b2Body.b2_kinematicBody;
+            
+            for(var i = 0; i < 100; ++i) {
+                if(Math.random() > 0.5) {
+                    Game.fixDef.shape = new b2PolygonShape;
+                    Game.fixDef.shape.SetAsBox(
+                        Math.random() + 0.1 //half width
+                        , Math.random() + 0.1 //half height
+                    );
+                } else {
+                    Game.fixDef.shape = new b2CircleShape(
+                        Math.random() + 0.1 //radius
+                    );
+                }
+                var random = (Math.random() > 0.3) ? b2Body.b2_dynamicBody : b2Body.b2_staticBody;
+                Game.bodyDef.type = random;
+                Game.bodyDef.position.x = Math.random() * 20 + 20;
+                Game.bodyDef.position.y = Math.random() * 20;
+                Game.world.CreateBody(Game.bodyDef).CreateFixture(Game.fixDef);
+            }
+        },
+        testmap: [
+            {
+                posX: 20,
+                posY: 20,
+                width: 1,
+                height: 1
+            },
+            {
+                posX: 20,
+                posY: 19,
+                width: 2,
+                height: 2
+            },
+            {
+                posX: 20,
+                posY: 17.5,
+                width: 3,
+                height: 3
+            },
+            {
+                posX: 20,
+                posY: 15.5,
+                width: 4,
+                height: 4
+            },
+            {
+                posX: 20,
+                posY: 13,
+                width: 5,
+                height: 5
+            },
+            {
+                posX: 19,
+                posY: 8,
+                width: 1,
+                height: 1
+            },
+            {
+                posX: 21,
+                posY: 8,
+                width: 1,
+                height: 1,
+                angryType: 3
+            }
+            
+        ]
+
+    },
+
+    init: function (game) {
+        this.game = game;
+    },
+
+    makeMap: function (mapID) {
+        var Game = this.game;
+        if(typeof mapID === 'undefined') {
+            this.maps.random.apply(this);
+            return;
+        }
+        var Map = this.maps[mapID];
+        var b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+            b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
+            b2Body = Box2D.Dynamics.b2Body;
+        
+        //create some objects
+        Game.bodyDef.type = b2Body.b2_dynamicBody;
+        for (var i = 0; i < Map.length; ++i) {
+            var elementObject = Map[i];
+
+            //Object angryType - if can be destroyed etc.
+            if(elementObject.angryType) {
+                Game.bodyDef.angryType = elementObject.angryType;
+            } else {
+                Game.bodyDef.angryType = Game.angryTypeBrick;
+            }
+            //Object which destroys other objects
+            if(elementObject.bullet) {
+                Game.bodyDef.bullet = elementObject.bullet;
+            } else {
+                Game.bodyDef.bullet = false;
+            }
+            if(elementObject.angryType === Game.angryTypeIE) {
+                Game.fixDef.shape = new b2CircleShape(
+                    0.8 //radius
+                );
+            } else {
+                Game.fixDef.shape = new b2PolygonShape;
+                Game.fixDef.shape.SetAsBox(
+                    elementObject.width/2, //half width
+                    elementObject.height/2  //half height
+                );
+            }
+
+            Game.bodyDef.position.x = elementObject.posX;
+            Game.bodyDef.position.y = elementObject.posY-0.7;
+            Game.world.CreateBody(Game.bodyDef).CreateFixture(Game.fixDef);
+            if(elementObject.angryType == Game.angryTypeIE) {
+                var aabb = new Box2D.Collision.b2AABB(), that = this;
+                aabb.lowerBound.Set(elementObject.posX - 0.001, elementObject.posY-0.7 - 0.001);
+                aabb.upperBound.Set(elementObject.posX + 0.001, elementObject.posY-0.7 + 0.001);
+                this.game.world.QueryAABB(function (fixture) {
+                    if(fixture.GetBody().GetType() != b2Body.b2_staticBody) {
+                       if(fixture.GetShape().TestPoint(fixture.GetBody().GetTransform(), new Box2D.Common.Math.b2Vec2(elementObject.posX,elementObject.posY-0.7))) {
+                          window.IE = fixture.GetBody();
+                          return false;
+                       }
+                    }
+                    return true;
+                 }, aabb);
+            }
+        }
+    }
+});
